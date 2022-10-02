aecea0c374cd07651977c7308caf3efccd1091b9
Sandun Induranga <88975401+Sandun-Induranga@users.noreply.github.com>
2022-10-02 09:33:04 +0530

    Add files via upload

---
+++ Calculator/assets/js/index.js
@@ -0,0 +1,63 @@
+let number = "";
+let firstNumber = 0;
+let lastNumber = 0;
+let operator = "";
+let topDisplay = "";
+
+for (let i = 0; i < 17; i++) {
+    $("#btn" + i).click(function () {
+        let pressedBtn = $("#btn" + i).text();
+        switch (pressedBtn) {
+            case "C":
+                firstNumber = "";
+                lastNumber = "";
+                operator = "";
+                number = "";
+                topDisplay = "";
+                break;
+            case "+":
+                setOutput("+");
+                break;
+            case "-":
+                setOutput("-");
+                break;
+            case "x":
+                setOutput("x");
+                break;
+            case "/":
+                setOutput("/");
+                break;
+            case "=":
+                lastNumber = parseFloat(number);
+                number = "";
+                let ans = operations(operator, firstNumber, lastNumber);
+                number = ans;
+                break;
+            default:
+                number += pressedBtn;
+                topDisplay += pressedBtn;
+        }
+        $(".display input:first-child").val(topDisplay);
+        $(".display input:last-child").val(number);
+    });
+}
+
+function operations(operator, firstNum, lastNum) {
+    switch (operator) {
+        case "+":
+            return firstNum + lastNum;
+        case "-":
+            return firstNum - lastNum;
+        case "x":
+            return firstNum * lastNum;
+        case "/":
+            return firstNum / lastNum;
+    }
+}
+
+function setOutput(pressedButton) {
+    firstNumber = parseFloat(number);
+    topDisplay += pressedButton;
+    number = "";
+    operator = pressedButton;
+}
